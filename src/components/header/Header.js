b9d60b4a4b33ff68eea0e9e47db4ca81286f20b2
Vangari2172 <ramushivshankar@gmail.com>
2022-11-03 12:37:23 +0530

    s

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from "../../images/logo.png";
 
 const Header = () => {
+  const [isClick, setIsClick] = useState(false);
+
+  const navBtn = () => {
+    return isClick ? setIsClick(false) : setIsClick(true);
+  };
+
   return (
-    <header className="header">
-      <nav className="navbar">
-        <img src={logo} alt="logo" className="logo" />
-        <div>
-          <ul className="main-nav">
+    <div>
+      <header className={isClick ? "header nav-open" : "header"}>
+        <div className="logo">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <nav className="main-nav">
+          <ul className="main-nav-list">
             <li>
-              <a className="list-name" href="#home">
+              <a className="main-nav-link" href="#home">
                 Home
               </a>
             </li>
-
             <li>
-              <a className="list-name" href="#about">
+              <a className="main-nav-link" href="#about">
                 About
               </a>
             </li>
-
             <li>
-              <a className="list-name" href="#services">
+              <a className="main-nav-link" href="#services">
                 Services
               </a>
             </li>
-
             <li>
-              <a className="list-name" href="#contact">
+              <a className="main-nav-link" href="#contact">
                 Contact Us
               </a>
             </li>
           </ul>
-        </div>
-        <div className="menu-bar">
+        </nav>
+        <button className="btn-mobile-nav" onClick={navBtn}>
           <i class="fa-solid fa-bars"></i>
-        </div>
-      </nav>
-      {/* <hr
-        style={{ border: "none", height: "5px", backgroundColor: "orangered" }}
-      /> */}
-    </header>
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </header>
+    </div>
   );
 };
 
