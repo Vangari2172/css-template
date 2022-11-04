cd9e1a5baac1d7d97eb51077416c397b9717844a
Vangari2172 <ramushivshankar@gmail.com>
2022-11-04 12:40:00 +0530

    a

--- src/components/section6/Section6.js
+++ src/components/section6/Section6.js
@@ -1,36 +1,94 @@
 import React from "react";
 import "./Section6.css";
 import sec6_img from "../../images/face.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Section6 = () => {
+  var settings = {
+    infinite: true,
+    speed: 500,
+    // autoplay: true,
+    autoplayspeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="section6">
-      <h1>Testimonials</h1>
-      <p className="sec6_p">Lorem Ipsum is simply dummy</p>
-      <div className="container grid-section6">
-        {/* <div className="first-div">
-          <p className="first-para">Lorem Ipsem</p>
-          <p className="third-des">Lorem Ipsem</p>
-        </div> */}
-        <div className="second-div">
-          <div className="sec6_image">
-            <img src={sec6_img} alt="face" />
+      <div className="container">
+        <h1>Testimonials</h1>
+        <p className="sec6_p">Lorem Ipsum is simply dummy</p>
+        {/* <Slider {...settings}>
+          <div>
+            <h3>1</h3>
           </div>
-          <blockquote>
-            "Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            ""
-          </blockquote>
-          <p className="quote">&mdash; Hayley Alex</p>
-          <p className="location">London</p>
-        </div>
-        {/* <div className="third-div">
-          <p className="third-para">Lorem Ipsem</p>
-          <p className="third-des">Lorem Ipsem</p>
-        </div> */}
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider> */}
+        <Slider {...settings}>
+          <div className="second-div">
+            <blockquote>
+              "Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, ""
+            </blockquote>
+            <div className="sec6_image">
+              <img src={sec6_img} alt="face" />
+            </div>
+            <p className="quote">&mdash; Hayley Alex</p>
+            <p className="location">London</p>
+          </div>
+
+          <div className="second-div">
+            <blockquote>
+              "Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, ""
+            </blockquote>
+            <div className="sec6_image">
+              <img src={sec6_img} alt="face" />
+            </div>
+            <p className="quote">&mdash; Hayley Alex</p>
+            <p className="location">London</p>
+          </div>
+
+          <div className="second-div">
+            <blockquote>
+              "Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, ""
+            </blockquote>
+            <div className="sec6_image">
+              <img src={sec6_img} alt="face" />
+            </div>
+            <p className="quote">&mdash; Hayley Alex</p>
+            <p className="location">London</p>
+          </div>
+        </Slider>
       </div>
     </div>
   );
